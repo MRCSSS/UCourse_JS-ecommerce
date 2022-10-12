5b02b2661a5d5ee716f5ae0f0a921eeed2c1abdb
Marcos Solis S <mss@192.168.1.12>
2022-10-11 19:38:33 -0500

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 import Productos from './components/Productos';
+import Title from './components/Title';
 
 class App extends Component {
   state = {
@@ -7,15 +10,59 @@ class App extends Component {
       { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
       { name: 'Arvejas', price: 2500, img: '/productos/arvejas.jpg' },
       { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
-    ]
+    ],
+    cart: [],
+    isCartVisible: false,
   }
+
+  addToCart = (product) => {
+    // console.log(product);
+    const { cart } = this.state
+
+    if (cart.find(x => x.name === product.name)) {
+      const newCart = cart.map(x => x.name === product.name
+        ? ({
+          ...x,
+          quantity: x.quantity + 1
+        })
+        : x
+      )
+      
+      return this.setState({ cart: newCart })
+    }
+
+    return this.setState({
+      cart: this.state.cart.concat({
+        ...product,
+        quantity: 1,
+      })
+    })
+  }
+
+  showCart = () => {
+    if (!this.state.cart.length) {
+      return
+    }
+    this.setState({ isCartVisible: !this.state.isCartVisible })
+  }
+
   render() {
+    const { isCartVisible } = this.state
+
     return (
       <div>
-        <Productos 
-          agregarAlCarro={() => console.log('Nada')}
-          productos={this.state.productos}
+        <Navbar 
+          cart={this.state.cart}
+          isCartVisible={isCartVisible}
+          showCart={this.showCart} 
         />
+        <Layout>
+          <Title />
+          <Productos 
+            agregarAlCarro={this.addToCart}
+            productos={this.state.productos}
+          />
+        </Layout>
       </div>
     )
   }
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,13 +1,33 @@
 import { Component } from "react";
+import Button from './Button';
+
+const styles = {
+    producto: {
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px',
+        margin: '5px 15px'
+
+    },
+    img: {
+        width: '75%',
+        height: '150px',
+    }
+}
 
 class Producto extends Component {
     render() {
-        const { producto } = this.props;
+        const { producto, agregarAlCarro } = this.props;
         return (
-            <div>
-                <img alt={producto.name} src={producto.img} />
+            <div style={styles.producto}>
+                <img alt={producto.name} src={producto.img} style={styles.img} />
                 <h3>{producto.name}</h3>
                 <p>$ {producto.price}.00</p>
+                <Button onClick={() => agregarAlCarro(producto)}>
+                    Agregar al carro
+                </Button>
             </div>
         )
     }
